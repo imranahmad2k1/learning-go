e3e1eabebb55bbe53f7081815bf1364bfebab376
Imran Ahmad <imranahmad2k1@gmail.com>
2024-01-24 00:57:03 +0500

    Basic server + serving some pages

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
 
 func main() {
-	fmt.Println("Hello World")
+	tpl, _ = tpl.ParseGlob("templates/*.html")
+
+	http.HandleFunc("/home", homepageFunction)
+	http.HandleFunc("/hello", helloFunction)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloFunction(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "Hello, World")
+	tpl.ExecuteTemplate(w, "hello.html", nil)
+}
+
+func homepageFunction(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "homepage, r.URL.Path: %s!", r.URL.Path)
+	tpl.ExecuteTemplate(w, "homepage.html", nil)
 }
